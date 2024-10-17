095edd4e65e8b0a973ca6d9a1274b05ec4299906
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-17 16:05:07 +0300

    Alx interview | Min Operations fix 7

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -20,11 +20,15 @@ def minOperations(n):
     Calc least number of operation (Copy all and paste)
     to have n chars of H
     """
-    if n <= 1:
+    if n < 2:
         return 0
+
     operations = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
-            operations += i
-            n //= i
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
     return operations
