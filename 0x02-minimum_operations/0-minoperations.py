5ef4dc62a7d4c0b10ebcba221d6c85eaa233c003
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-17 15:29:31 +0300

    Alx interview | Min Operations fix 6

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,25 +12,6 @@ MinOperations :
     -   Returns an integer
     -   If n is impossible to achieve, return 0
 """
-from math import sqrt
-
-
-def isPrime(n: int) -> bool:
-    """
-    isPrime(n):
-    Check if the number is prime or not
-    """
-    if (n <= 1):
-        return False
-    elif (n == 2 or n == 3):
-        return True
-    elif (n % 2 == 0 or n % 3 == 0):
-        return False
-    else:
-        for i in range(5, int(sqrt(n)) + 1, 6):
-            if (n % i == 0):
-                return False
-    return True
 
 
 def minOperations(n):
@@ -39,10 +20,11 @@ def minOperations(n):
     Calc least number of operation (Copy all and paste)
     to have n chars of H
     """
-
+    if n <= 1:
+        return 0
     operations = 0
     for i in range(2, n + 1):
         while n % i == 0:
             operations += i
             n //= i
-    return operations if n == 1 else 0
+    return operations
