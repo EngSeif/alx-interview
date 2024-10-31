f8a8ad44708fa7ac80cf5583da806fc809b25832
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-31 15:00:00 +0300

    interview | UTF-8 fix 4

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,26 +11,28 @@ def validUTF8(data):
     """
     i = 0
     while i < len(data):
-        # Determine the number of bytes in the current UTF-8 character
         byte = data[i]
-        if byte >> 7 == 0:          # 1-byte character (0xxxxxxx)
+
+        # Determine the number of bytes in the UTF-8 character
+        if byte >> 7 == 0:  # 1-byte character (0xxxxxxx)
             num_bytes = 1
-        elif byte >> 5 == 0b110:    # 2-byte character (110xxxxx)
+        elif byte >> 5 == 0b110:  # 2-byte character (110xxxxx)
             num_bytes = 2
-        elif byte >> 4 == 0b1110:   # 3-byte character (1110xxxx)
+        elif byte >> 4 == 0b1110:  # 3-byte character (1110xxxx)
             num_bytes = 3
         elif byte >> 3 == 0b11110:  # 4-byte character (11110xxx)
             num_bytes = 4
-        else:                       # Invalid starting byte
+        else:  # Invalid starting byte pattern
             return False
 
-        # Check if there are enough bytes remaining for this character
+        # Check if there are enough bytes remaining in the data
         if i + num_bytes > len(data):
             return False
 
-        # Validate each continuation byte (must start with '10')
+        # Validate continuation bytes: they must all start with '10'
         for j in range(1, num_bytes):
-            if data[i + j] >> 6 != 0b10:
+            continuation_byte = data[i + j]
+            if continuation_byte >> 6 != 0b10:  # '10xxxxxx' pattern check
                 return False
 
         # Move to the next character in the data
