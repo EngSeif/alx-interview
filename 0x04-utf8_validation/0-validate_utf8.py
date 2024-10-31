584b0ed6e68d1af7a1ae11714f97821e98c4e695
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-31 15:22:31 +0300

    interview | UTF-8 fix 6

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,31 +11,28 @@ def validUTF8(data):
     """
     i = 0
     while i < len(data):
-        byte = data[i]
-
-        # Determine the number of bytes in the UTF-8 character
-        if byte >> 7 == 0:  # 1-byte character (0xxxxxxx)
-            num_bytes = 1
-        elif byte >> 5 == 0b110:  # 2-byte character (110xxxxx)
-            num_bytes = 2
-        elif byte >> 4 == 0b1110:  # 3-byte character (1110xxxx)
-            num_bytes = 3
-        elif byte >> 3 == 0b11110:  # 4-byte character (11110xxx)
+        byte = bin(data[i])[2:].zfill(8)
+        print(byte[0:1])
+        if byte[0:5] == '11110':
             num_bytes = 4
-        else:  # Invalid starting byte pattern
+        elif byte[0:4] == '1110':
+            num_bytes = 3
+        elif byte[0:3] == '110':
+            num_bytes = 2
+        elif byte[0:2] == '10':
+            return False
+        elif byte[0:1] == '0':
+            i += 1
+            continue
+        else:
             return False
 
-        # Check if there are enough bytes remaining in the data
-        if i + num_bytes >= len(data):
+        if i + num_bytes > len(data):
             return False
 
-        # Validate continuation bytes: they must all start with '10'
         for j in range(1, num_bytes):
-            continuation_byte = data[i + j]
-            if continuation_byte >> 6 != 0b10:  # '10xxxxxx' pattern check
+            cont_byte = bin(data[i + j])[2:].zfill(8)
+            if cont_byte[:2] != '10':
                 return False
-
-        # Move to the next character in the data
         i += num_bytes
-
     return True
