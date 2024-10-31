41e596a1f7ea8f80af8f4327e6027bf9a4728622
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-31 19:11:53 +0300

    interview | UTF-8 fix 18

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,31 +2,32 @@
 """UTF-8 Validation"""
 
 
-def get_leading_set_bits(num):
-    """returns the number of leading set bits (1)"""
-    set_bits = 0
-    helper = 1 << 7
-    while helper & num:
-        set_bits += 1
-        helper = helper >> 1
-    return set_bits
-
-
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
-    bits_count = 0
-    for i in range(len(data)):
-        if bits_count == 0:
-            bits_count = get_leading_set_bits(data[i])
-            '''1-byte (format: 0xxxxxxx)'''
-            if bits_count == 0:
+    """
+    validate if a set of data
+    is a valid utf-8 code
+    """
+    def countOnes(num):
+        """
+        Get number of leading ones
+        """
+        mask = 1 << 7
+        i = 0
+        while num & mask:
+            i += 1
+            mask = mask >> 1
+        return i
+    count = 0
+    for d in data:
+        if count == 0:
+            count = countOnes(d)
+            if count == 0:
                 continue
-            '''a character in UTF-8 can be 1 to 4 bytes long'''
-            if bits_count == 1 or bits_count > 4:
+            if count == 1 or count > 4:
                 return False
+            count -= 1
         else:
-            '''checks if current byte has format 10xxxxxx'''
-            if not (data[i] & (1 << 7) and not (data[i] & (1 << 6))):
+            count -= 1
+            if countOnes(d) != 1:
                 return False
-        bits_count -= 1
-    return bits_count == 0
+    return count == 0
