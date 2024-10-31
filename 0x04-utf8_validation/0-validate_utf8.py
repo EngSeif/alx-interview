09e72f55061e3a87d629651ad7440e73e758efe7
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-31 14:55:32 +0300

    interview | UTF-8 fix 3

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,30 +6,34 @@ Validate UTF-8 Data
 
 def validUTF8(data):
     """
-    determines if a given data set
+    Determines if a given data set
     represents a valid UTF-8 encoding.
     """
     i = 0
     while i < len(data):
-        binany_num = bin(data[i])[2:].zfill(8)
-        if binany_num[0] == '0':
-            i += 1
-            continue
-        elif binany_num[:3] == '110':
+        # Determine the number of bytes in the current UTF-8 character
+        byte = data[i]
+        if byte >> 7 == 0:          # 1-byte character (0xxxxxxx)
+            num_bytes = 1
+        elif byte >> 5 == 0b110:    # 2-byte character (110xxxxx)
             num_bytes = 2
-        elif binany_num[:4] == '1110':
+        elif byte >> 4 == 0b1110:   # 3-byte character (1110xxxx)
             num_bytes = 3
-        elif binany_num[:5] == '11110':
+        elif byte >> 3 == 0b11110:  # 4-byte character (11110xxx)
             num_bytes = 4
-        else:
+        else:                       # Invalid starting byte
             return False
 
-        if i + num_bytes >= len(data):
+        # Check if there are enough bytes remaining for this character
+        if i + num_bytes > len(data):
             return False
 
+        # Validate each continuation byte (must start with '10')
         for j in range(1, num_bytes):
-            binany_num = bin(data[i + j])[2:].zfill(8)
-            if binany_num[:2] != '10':
+            if data[i + j] >> 6 != 0b10:
                 return False
+
+        # Move to the next character in the data
         i += num_bytes
+
     return True
