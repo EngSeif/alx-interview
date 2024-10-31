33a45faad8217e78d8c6b0a80e2d4f3eac88e70b
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-31 18:10:03 +0300

    interview | UTF-8 fix 14

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,6 @@
 Validate UTF-8 Data
 """
 
-
 def validUTF8(data):
     """
     Determines if a given data set
@@ -11,30 +10,27 @@ def validUTF8(data):
     """
     i = 0
     while i < len(data):
-        if data[i] > 255:
-            return False
-        byte = bin(data[i])[2:].zfill(8)
-        if byte.startswith('11110'):
-            num_bytes = 4
-        elif byte.startswith('1110'):
-            num_bytes = 3
-        elif byte.startswith('110'):
+        byte = data[i]
+        
+        # Determine the number of bytes to expect
+        if byte >> 5 == 0b110:  # 2-byte character
             num_bytes = 2
-        elif byte.startswith('10'):
-            return False
-        elif byte.startswith('0'):
+        elif byte >> 4 == 0b1110:  # 3-byte character
+            num_bytes = 3
+        elif byte >> 3 == 0b11110:  # 4-byte character
+            num_bytes = 4
+        elif byte >> 7 == 0:  # 1-byte character
             num_bytes = 1
         else:
             return False
-
+        
+        # Check if there are enough bytes left in data
         if i + num_bytes > len(data):
             return False
-
+        
+        # Validate continuation bytes
         for j in range(1, num_bytes):
-            if i + j >= len(data):
-                return False
-            cont_byte = bin(data[i + j])[2:].zfill(8)
-            if not cont_byte.startswith('10'):
+            if (data[i + j] >> 6) != 0b10:  # Check for 10xxxxxx
                 return False
 
         i += num_bytes
