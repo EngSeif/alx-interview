3d9a3a60ef61c997db96a12277edb5e64545fd71
EngSeif <eng.seifmohamed2004@gmail.com>
2024-11-14 00:50:07 +0200

    Interview | Starwars fix 2

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,27 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('request-promise');
 
 const Id = process.argv[2];
 
 const apiUrl = `https://swapi.dev/api/films/${Id}/`;
 
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.log('Error fetching data:', error);
-    return;
-  }
-
-  // Parse the JSON response
-  const movieData = JSON.parse(body);
-
-  if (response.statusCode === 200) {
-    // Get the list of characters
-    const characters = movieData.characters;
-
-    // Print each character URL
-    characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.log('Error fetching character data:', error);
-          return;
-        }
-
-        const characterData = JSON.parse(body);
-        console.log(characterData.name);
-      });
+async function fetchData (url) {
+  try {
+    const filmResponse = await request({
+      url,
+      json: true
     });
-  } else {
-    console.log('Error: Could not fetch movie data.');
+    const charUrls = filmResponse.characters;
+    for (let i = 0; i < charUrls.length; i++) {
+      const url = charUrls[i];
+      const charResponse = await request({
+        url,
+        json: true
+      });
+      console.log(charResponse.name);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-});
+}
+fetchData(apiUrl);
